643887fa6b5cfd478d0f99e640c75bb7f0a1b247
GabriG23 <greco.gabriele.1996@gmail.com>
2023-01-19 15:53:58 +0100

    altri update

--- cosface_loss.py
+++ cosface_loss.py
@@ -4,6 +4,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import Parameter
+import math
 
 # SphereFace: devo moltiplicare m all'angolo theta
 # ArcFace: devo sommare m all'angolo theta
@@ -48,6 +49,20 @@ class MarginCosineProduct(nn.Module): # CosFace
     # tensor.scatter(dim, index, src, reduce = None) = scrive tutti i valori dal tensore src dentro self agli indici specificati del tensore
     # tensor.view() = ritorna un nuovo tensor con gli stessi dati del self tensor ma con forma diversa
 
+#     def forward(self, x, y):
+#         with torch.no_grad():
+#             self.w.data = F.normalize(self.w.data, dim=0)
+
+#         cos_theta = F.normalize(x, dim=1).mm(self.w)
+#         with torch.no_grad():
+#             d_theta = torch.zeros_like(cos_theta)
+#             d_theta.scatter_(1, y.view(-1, 1), -self.m, reduce='add')
+
+#         logits = self.s * (cos_theta + d_theta)
+#         loss = F.cross_entropy(logits, y)
+
+#         return loss
+
     def forward(self, inputs: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
         cosine = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
         one_hot = torch.zeros_like(cosine) # tensor di 0 con la stessa dimensione di cosine
@@ -64,7 +79,7 @@ class MarginCosineProduct(nn.Module): # CosFace
                + ', m=' + str(self.m) + ')'
 
 #################### ArcFace (ArcFace) ###############################################
-# SECONDA VERSIONE GIUSTA
+
 class ArcFace(nn.Module):
     """Implement of large margin cosine distance:
     Args:
@@ -73,7 +88,7 @@ class ArcFace(nn.Module):
         s: norm of input feature
         m: margin
     """
-    def __init__(self, in_features: int, out_features: int, s: float = 64.0, m: float = 0.50): # modificati da s = 30, m = 0.4
+    def __init__(self, in_features: int, out_features: int, s: float = 64.0, m: float = 0.5): # modificati da s = 30, m = 0.4
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -85,17 +100,22 @@ class ArcFace(nn.Module):
     # tensor.where(condition, x, y) ritorna un tensor di element selezionati da x o y, dipendente dalla condizione
     # tensor.acos(input, *, out = None) = calcola l'inverso del coseno per ogni elemento in input
     # tensor.cos(input, *, out = None) = calcola il coseno per ogni elemento in input
+    # tensor.no_grad(): context-manager that disabled gradient calculation. Disabling gradient calculation is useful for inference, when you are sure that you will not call tensor.backward().
+    #                   It will reduce memory consuption for computations that would otherwise have requires_grad=True
 
     def forward(self, inputs: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-        cosine = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
-        one_hot = torch.zeros_like(cosine) # tensor di 0 con la stessa dimensione di cosine
-        one_hot.scatter_(1, label.view(-1, 1), 1.0)
-        one_hot.mul_(self.m)
-        cosine.acos_() # ora ho un tensor arccos di cosine
-        cosine += one_hot
-        cosine.cos_() # ritrasformo in cosine
-        output = self.s * cosine
-        return output
+        cos_theta = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
+
+        with torch.no_grad():
+            theta_m = torch.acos(cos_theta.clamp(-1+1e-5, 1-1e-5)) # clamp serve? non lo faccio già nella cosine_sim?
+            theta_m.scatter_(1, label.view(-1, 1), self.m, reduce='add')
+            theta_m.clamp_(1e-5, 3.14159)
+            d_theta = torch.cos(theta_m) - cos_theta
+
+        logits = self.s * (cos_theta + d_theta)
+        # loss = F.cross_entropy(logits, label)
+        # return loss
+        return logits
     
     def __repr__(self):
         return self.__class__.__name__ + '(' \
@@ -103,50 +123,6 @@ class ArcFace(nn.Module):
                + ', out_features=' + str(self.out_features) \
                + ', s=' + str(self.s) \
                + ', m=' + str(self.m) + ')'
-
-
-# PRIMA VERSIONE SBAGLIATA
-# class ArcFace(nn.Module):
-#     """Implement of large margin cosine distance:
-#     Args:
-#         in_features: size of each input sample
-#         out_features: size of each output sample
-#         s: norm of input feature
-#         m: margin
-#     """
-#     def __init__(self, in_features: int, out_features: int, s: float = 64.0, m: float = 0.40): # modificati da s = 30, m = 0.4
-#         super().__init__()
-#         self.in_features = in_features
-#         self.out_features = out_features
-#         self.s = s
-#         self.m = m
-#         self.weight = Parameter(torch.Tensor(out_features, in_features))
-#         nn.init.xavier_uniform_(self.weight)
-    
-#     # tensor.where(condition, x, y) ritorna un tensor di element selezionati da x o y, dipendente dalla condizione
-#     # tensor.acos(input, *, out = None) = calcola l'inverso del coseno per ogni elemento in input
-#     # tensor.cos(input, *, out = None) = calcola il coseno per ogni elemento in input
-
-#     def forward(self, inputs: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-#         cosine = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
-#         # invece di sottrarre m al cosine, devo aggiungerlo all'angolo del cosine
-#         # one_hot = torch.zeros_like(cosine)
-#         # one_hot.scatter_(1, label.view(-1, 1), self.m) # 1 al posto self.m
-#         cosine.acos_() # ora ho un tensor arccos di cosine
-#         # se io sommo due tensori, ottengo un matrice, io non voglio questo
-#         # cosine += one_hot # * self.m # sommo m
-#         cosine.add(self.m) # aggiungo m all'angolo
-#         cosine.cos() # ritrasformo in cosine
-#         cosine.mul(self.s) # moltiplico per s
-#         # output = self.s * cosine 
-#         return cosine
-    
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(' \
-#                + 'in_features=' + str(self.in_features) \
-#                + ', out_features=' + str(self.out_features) \
-#                + ', s=' + str(self.s) \
-#                + ', m=' + str(self.m) + ')'
 
 #################### SphereFace (A-softmax) ###############################################
 
@@ -163,7 +139,7 @@ class SphereFace(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
         self.s = s
-        self.m = m # per sphereface, m>=1, bisogna capire questo valore di m, vedere 3.4 del paper
+        self.m = m # m >= 1
         self.weight = Parameter(torch.Tensor(out_features, in_features))
         nn.init.xavier_uniform_(self.weight)
     
@@ -172,16 +148,20 @@ class SphereFace(nn.Module):
     # tensor.cos(input, *, out = None) = calcola il coseno per ogni elemento in input
 
     def forward(self, inputs: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-        cosine = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
-        one_hot = torch.zeros_like(cosine) # tensor di 0 con la stessa dimensione di cosine
-        one_hot.scatter_(1, label.view(-1, 1), 1.0)
-        one_hot.mul_(self.m)
-        cosine.acos_() # ora ho un tensor arccos di cosine
-        cosine.mul_(one_hot) # da vedere se funziona
-        cosine.cos_()# ritrasformo in cosine
-        output = self.s * cosine
-        return output
-    
+        cos_theta = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
+
+        with torch.no_grad():
+            m_theta = torch.acos(cos_theta.clamp(-1.+1e-5, 1.-1e-5))
+            m_theta.scatter_(1, label.view(-1, 1), self.m, reduce = 'multiply')
+
+            k = (m_theta / math.pit).floor()
+            sign = -2 * torch.remainder(k, 2) + 1 # (-1)**k
+            phi_theta = sign * torch.cos(m_theta) - 2. * k
+            d_theta = phi_theta - cos_theta
+
+        logits = self.s * (cos_theta + d_theta)
+        return logits
+
     def __repr__(self):
         return self.__class__.__name__ + '(' \
                + 'in_features=' + str(self.in_features) \
@@ -190,171 +170,101 @@ class SphereFace(nn.Module):
                + ', m=' + str(self.m) + ')'
 
 
-#################### ArcFace (ArcFace) ###############################################
-# Ho trovato queste implementazioni di cosFace e ArcFace 
-# Based on https://github.com/deepinsight/insightface/blob/master/recognition/vpl/losses.py
- 
-# def get_loss(name):
-#     if name == "cosface":
-#         return CosFace()
-#     elif name == "arcface":
-#         return ArcFace()
-#     else:
-#         raise ValueError()
+# IMPLEMENTAZIONE COSFACE, ARCFACE e SPHERE FACE: https://opensphere.world/
+
+#################### CosFace ##############################################################
 
 # class CosFace(nn.Module):
-#     def __init__(self, s=64.0, m=0.40):
+#     """reference1: <CosFace: Large Margin Cosine Loss for Deep Face Recognition>
+#        reference2: <Additive Margin Softmax for Face Verification>
+#     """
+#     def __init__(self, feat_dim, num_class, s=64., m=0.35):
 #         super(CosFace, self).__init__()
+#         self.feat_dim = feat_dim
+#         self.num_class = num_class
 #         self.s = s
 #         self.m = m
+#         self.w = nn.Parameter(torch.Tensor(feat_dim, num_class))
+#         nn.init.xavier_normal_(self.w)
 
-#     def forward(self, cosine, label):
-#         index = torch.where(label != -1)[0]
-#         m_hot = torch.zeros(index.size()[0], cosine.size()[1], device=cosine.device)
-#         m_hot.scatter_(1, label[index, None], self.m)
-#         cosine[index] -= m_hot
-#         ret = cosine * self.s
-#         return ret
+#     def forward(self, x, y):
+#         with torch.no_grad():
+#             self.w.data = F.normalize(self.w.data, dim=0)
 
+#         cos_theta = F.normalize(x, dim=1).mm(self.w)
+#         with torch.no_grad():
+#             d_theta = torch.zeros_like(cos_theta)
+#             d_theta.scatter_(1, y.view(-1, 1), -self.m, reduce='add')
 
-# class ArcFace(nn.Module):
-#     def __init__(self, s=64.0, m=0.5):
-#         super(ArcFace, self).__init__()
-#         self.s = s
-#         self.m = m
+#         logits = self.s * (cos_theta + d_theta)
+#         loss = F.cross_entropy(logits, y)
 
-#     def forward(self, cosine: torch.Tensor, label):
-#         index = torch.where(label != -1)[0]
-#         m_hot = torch.zeros(index.size()[0], cosine.size()[1], device=cosine.device)
-#         m_hot.scatter_(1, label[index, None], self.m)
-#         cosine.acos_()
-#         cosine[index] += m_hot
-#         cosine.cos_().mul_(self.s)
-#         return cosine
- 
-
-
-#################### SphereFace (A-softmax) ###############################################
-# Ho trovato queste implementazioni di SphereFace
-# Based on https://github.com/clcarwin/sphereface_pytorch/blob/master/net_sphere.py
- 
-# class AngleLoss(nn.Module):
-#     def __init__(self, gamma=0):
-#         super(AngleLoss, self).__init__()
-#         self.gamma   = gamma
-#         self.it = 0
-#         self.LambdaMin = 5.0
-#         self.LambdaMax = 1500.0
-#         self.lamb = 1500.0
- 
-#     def forward(self, input, target):
-#         self.it += 1
-#         cos_theta,phi_theta = input
-#         target = target.view(-1,1) #size=(B,1)
- 
-#         index = cos_theta.data * 0.0 #size=(B,Classnum)
-#         index.scatter_(1,target.data.view(-1,1),1)
-#         index = index.byte()
-#         index = Variable(index)
- 
-#         self.lamb = max(self.LambdaMin,self.LambdaMax/(1+0.1*self.it ))
-#         output = cos_theta * 1.0 #size=(B,Classnum)
-#         output[index] -= cos_theta[index]*(1.0+0)/(1+self.lamb)
-#         output[index] += phi_theta[index]*(1.0+0)/(1+self.lamb)
- 
-#         logpt = F.log_softmax(output)
-#         logpt = logpt.gather(1,target)
-#         logpt = logpt.view(-1)
-#         pt = Variable(logpt.data.exp())
- 
-#         loss = -1 * (1-pt)**self.gamma * logpt
-#         loss = loss.mean()
- 
 #         return loss
 
+#################### ArcFace ##############################################################
 
-# class AngleLinear(nn.Module):
-#     def __init__(self, in_features, out_features, m = 4, phiflag=True):
-#         super(AngleLinear, self).__init__()
-#         self.in_features = in_features
-#         self.out_features = out_features
-#         self.weight = Parameter(torch.Tensor(in_features,out_features))
-#         self.weight.data.uniform_(-1, 1).renorm_(2,1,1e-5).mul_(1e5)
-#         self.phiflag = phiflag
+# class ArcFace(nn.Module):
+#     """ reference: <Additive Angular Margin Loss for Deep Face Recognition>
+#     """
+#     def __init__(self, feat_dim, num_class, s=64., m=0.5):
+#         super(ArcFace, self).__init__()
+#         self.feat_dim = feat_dim
+#         self.num_class = num_class
+#         self.s = s
 #         self.m = m
-#         self.mlambda = [
-#             lambda x: x**0,
-#             lambda x: x**1,
-#             lambda x: 2*x**2-1,
-#             lambda x: 4*x**3-3*x,
-#             lambda x: 8*x**4-8*x**2+1,
-#             lambda x: 16*x**5-20*x**3+5*x
-#         ]
+#         self.w = nn.Parameter(torch.Tensor(feat_dim, num_class))
+#         nn.init.xavier_normal_(self.w)
 
-#     def forward(self, input):
-#         x = input   # size=(B,F)    F is feature len
-#         w = self.weight # size=(F,Classnum) F=in_features Classnum=out_features
+#     def forward(self, x, y):
+#         with torch.no_grad():
+#             self.w.data = F.normalize(self.w.data, dim=0)
 
-#         ww = w.renorm(2,1,1e-5).mul(1e5)
-#         xlen = x.pow(2).sum(1).pow(0.5) # size=B
-#         wlen = ww.pow(2).sum(0).pow(0.5) # size=Classnum
+#         cos_theta = F.normalize(x, dim=1).mm(self.w)
+#         with torch.no_grad():
+#             theta_m = torch.acos(cos_theta.clamp(-1+1e-5, 1-1e-5))
+#             theta_m.scatter_(1, y.view(-1, 1), self.m, reduce='add')
+#             theta_m.clamp_(1e-5, 3.14159)
+#             d_theta = torch.cos(theta_m) - cos_theta
 
-#         cos_theta = x.mm(ww) # size=(B,Classnum)
-#         cos_theta = cos_theta / xlen.view(-1,1) / wlen.view(1,-1)
-#         cos_theta = cos_theta.clamp(-1,1)
+#         logits = self.s * (cos_theta + d_theta)
+#         loss = F.cross_entropy(logits, y)
 
-#         if self.phiflag:
-#             cos_m_theta = self.mlambda[self.m](cos_theta)
-#             theta = Variable(cos_theta.data.acos())
-#             k = (self.m*theta/3.14159265).floor()
-#             n_one = k*0.0 - 1
-#             phi_theta = (n_one**k) * cos_m_theta - 2*k
-#         else:
-#             theta = cos_theta.acos()
-#             phi_theta = myphi(theta,self.m)
-#             phi_theta = phi_theta.clamp(-1*self.m,1)
+#         return loss
 
-#         cos_theta = cos_theta * xlen.view(-1,1)
-#         phi_theta = phi_theta * xlen.view(-1,1)
-#         output = (cos_theta,phi_theta)
-#         return output # size=(B,Classnum,2)
-
+#################### SphereFace ##############################################################
 
 # class SphereFace(nn.Module):
-#     """Implement of large margin cosine distance:
-#     Args:
-#         in_features: size of each input sample
-#         out_features: size of each output sample
-#         s: norm of input feature
-#         m: margin
+#     """ reference: <SphereFace: Deep Hypersphere Embedding for Face Recognition>"
+#         It also used characteristic gradient detachment tricks proposed in
+#         <SphereFace Revived: Unifying Hyperspherical Face Recognition>.
 #     """
-#     def __init__(self, in_features: int, out_features: int, s: float = 30.0, m: float = 1.5): # modificati da s = 30, m = 0.4
-#         super().__init__()
-#         self.in_features = in_features
-#         self.out_features = out_features
+#     def __init__(self, feat_dim, num_class, s=30., m=1.5):
+#         super(SphereFace, self).__init__()
+#         self.feat_dim = feat_dim
+#         self.num_class = num_class
 #         self.s = s
-#         self.m = m # per sphereface, m>=1, bisogna capire questo valore di m, vedere 3.4 del paper
-#         self.weight = Parameter(torch.Tensor(out_features, in_features))
-#         nn.init.xavier_uniform_(self.weight)
-    
-#     # tensor.where(condition, x, y) ritorna un tensor di element selezionati da x o y, dipendente dalla condizione
-#     # tensor.acos(input, *, out = None) = calcola l'inverso del coseno per ogni elemento in input
-#     # tensor.cos(input, *, out = None) = calcola il coseno per ogni elemento in input
+#         self.m = m
+#         self.w = nn.Parameter(torch.Tensor(feat_dim, num_class))
+#         nn.init.xavier_normal_(self.w)
 
-#     def forward(self, inputs: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
-#         cosine = cosine_sim(inputs, self.weight) # calcola la cosine similarity, cos(theta)
-#         # invece di sottrarre m al cosine, devo aggiungerlo all'angolo del cosine
-#         # one_hot = torch.zeros_like(cosine) #
-#         # one_hot.scatter_(1, label.view(-1, 1), 1.0)
-#         cosine.acos_() # ora ho un tensor arccos di cosine
-#         cosine.mul(self.m) # dovrebbe essere giusto, non ho ancora ben capito al 100% la questione del one_hot
-#         cosine.cos_() # lo riporto normale
-#         cosine.mul(self.s)
-#         return cosine
-    
-#     def __repr__(self):
-#         return self.__class__.__name__ + '(' \
-#                + 'in_features=' + str(self.in_features) \
-#                + ', out_features=' + str(self.out_features) \
-#                + ', s=' + str(self.s) \
-#                + ', m=' + str(self.m) + ')'
+#     def forward(self, x, y):
+#         # weight normalization
+#         with torch.no_grad():
+#             self.w.data = F.normalize(self.w.data, dim=0)
+
+#         # cos_theta and d_theta
+#         cos_theta = F.normalize(x, dim=1).mm(self.w)
+#         with torch.no_grad():
+#             m_theta = torch.acos(cos_theta.clamp(-1.+1e-5, 1.-1e-5))
+#             m_theta.scatter_(
+#                 1, y.view(-1, 1), self.m, reduce='multiply',
+#             )
+#             k = (m_theta / math.pi).floor()
+#             sign = -2 * torch.remainder(k, 2) + 1  # (-1)**k
+#             phi_theta = sign * torch.cos(m_theta) - 2. * k
+#             d_theta = phi_theta - cos_theta
+
+#         logits = self.s * (cos_theta + d_theta)
+#         loss = F.cross_entropy(logits, y)
+
+#         return loss
