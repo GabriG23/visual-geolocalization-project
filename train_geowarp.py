97afd56026cc1616d9899b136ef2cd2abddc7853
GabriG23 <greco.gabriele.1996@gmail.com>
2023-01-29 19:12:36 +0100

    Update train_geowarp.py

--- train_geowarp.py
+++ train_geowarp.py
@@ -192,15 +192,13 @@ for epoch_num in range(start_epoch_num, args.epochs_num):        # inizia il tra
                             
             if args.ss_w != 0:  # ss_loss  # self supervised loss    guides the network to learn to estimate the points 
                 pred_warped_intersection_points_1 = model("regression", similarity_matrix_1to2)
-                print(pred_warped_intersection_points_1)
                 pred_warped_intersection_points_2 = model("regression", similarity_matrix_2to1)
-                print(pred_warped_intersection_points_2)
                 ss_loss = (mse(pred_warped_intersection_points_1[:, :4], warped_intersection_points_1) +
                         mse(pred_warped_intersection_points_1[:, 4:], warped_intersection_points_2) +
                         mse(pred_warped_intersection_points_2[:, :4], warped_intersection_points_2) +
                         mse(pred_warped_intersection_points_2[:, 4:], warped_intersection_points_1))
                 print(ss_loss)
-                ss_loss.type(torch.float32)
+                ss_loss = ss_loss.type(torch.float32)
                 print(ss_loss)
                 # ss_loss *= args.ss_w        # applica il peso alla loss
                 ss_loss.backward()
