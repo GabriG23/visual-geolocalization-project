a75943e367dc0b6708e877c8055ccf5fc71b6bdc
Davide Aiello <dvdaiello98@gmail.com>
2023-05-26 10:55:44 +0200

    fix

--- local_features_utils.py
+++ local_features_utils.py
@@ -18,7 +18,8 @@ def CalculateReceptiveBoxes(height, width):
     x, y = torch.meshgrid(torch.arange(0, width), torch.arange(0, height))
     coordinates = torch.reshape(torch.stack([x, y], dim=2), [-1, 2])               #  ho girato x e y rispetto alla repo per fare uscire gli stessi valori
     # [y,x,y,x]
-    point_boxes = torch.tensor(torch.concat([coordinates, coordinates], 1), dtype=torch.float32)        
+    point_boxes = torch.concat([coordinates, coordinates], 1).to(torch.float32)        
+    # OPPURE    torch.concat([coordinates, coordinates], 1).to(torch.float32
 
     # point_boxes pare avere tutte le combinazioni possibili di coordinate che prevedono [y, x e y, x]    # anche se noi abbiamo messo x,y
     # ricontrollare in caso di errori
@@ -54,13 +55,13 @@ def retrieve_locations_descriptors(feature_map, attention_prob):
     feature_map = feature_map.view(-1, feature_map.shape[0])          
 
     abs_thres = 0.5
+    # abs_thres = 0.2
 
     indices = (attention_prob >= abs_thres).nonzero().squeeze(1) 
    
 
     selected_boxes = torch.index_select(rf_boxes, 0, indices)
     selected_features = torch.index_select(feature_map, 0, indices)
-    
     selected_scores = torch.index_select(attention_prob, 0, indices)
 
     scale = 1
@@ -87,6 +88,24 @@ def retrieve_locations_descriptors(feature_map, attention_prob):
     return locations, selected_features
 
 
+def plot_inlier_lines(query_im_array, index_im_array, query_locations, index_locations, inliers):
+    inlier_idxs = np.nonzero(inliers)[0]
+    
+    _, ax = plt.subplots()
+    ax.axis('off')
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.margins(0, 0)
+    
+    feature.plot_matches(ax, query_im_array, 
+                         index_im_array, 
+                         query_locations, 
+                         index_locations,
+                         np.column_stack((inlier_idxs, inlier_idxs)), 
+                         only_matches=True,)
+
+    plt.show()
+
+
 
 # Il KDTree viene utilizzato per accelerare la ricerca dei descrittori più vicini tra immagini diverse, 
 # mentre RANSAC viene utilizzato per eliminare i descrittori che non appartengono a un modello coerente, 
@@ -96,33 +115,29 @@ def retrieve_locations_descriptors(feature_map, attention_prob):
 
 def match_features(query_locations,
                   query_descriptors,
-                  database_image_locations,
-                  database_image_descriptors,
-                  ransac_seed=None,
-                  descriptor_matching_threshold=0.9,
+                  image_locations,
+                  image_descriptors,
+                  descriptor_matching_threshold=1.7,
                   ransac_residual_threshold=10.0,
                   query_im_array=None,
                   index_im_array=None,
-                  query_im_scale_factors=None,
-                  index_im_scale_factors=None,
                   use_ratio_test=False):
 
 
-    # NUM_TO_RERANK = 100                                         # numero massimo di immagini di cui fare il re-rank
-    _NUM_RANSAC_TRIALS = 500
+    _NUM_RANSAC_TRIALS = 100
     _MIN_RANSAC_SAMPLES = 3
 
     num_features_query = query_locations.shape[0]    
-    num_features_database_image = database_image_locations.shape[0]
-    if not num_features_query or not num_features_database_image:
+    num_features_image = image_locations.shape[0]
+    if not num_features_query or not num_features_image:
         print(f"database images or query image don't have consistent dimension")
 
-    local_feature_dim = query_descriptors.shape[1]              # queste dovrebbero essere 64
-    if database_image_descriptors.shape[1] != local_feature_dim:
+    local_feature_dim = query_descriptors.shape[1]       
+    if image_descriptors.shape[1] != local_feature_dim:
         print(f"Local feature dimensionality is not consistent for query and database images.")
 
     # Construct KD-tree used to find nearest neighbors.
-    index_image_tree = spatial.cKDTree(database_image_descriptors)
+    index_image_tree = spatial.cKDTree(image_descriptors)
 
 #   if use_ratio_test:                                  Se viene usato il ratio test. Possiamo provarlo in seguito
 #     distances, indices = index_image_tree.query(
@@ -149,62 +164,38 @@ def match_features(query_locations,
     # più vicini nello spazio multidimensionale. Questi putative matches devono essere successivamente verificati e filtrati 
     # per eliminare gli outliers e garantire che le corrispondenze siano accurate e affidabili.
 
-    query_locations_to_use = np.array([query_locations[i,] for i in range(num_features_query) if indices[i] != num_features_database_image])
+    matching_query_locations = np.array([query_locations[i,] for i in range(num_features_query) if indices[i] != num_features_image])
     # prende la locations di tutte le query per cui l'indice i-esimo è diverso dal numero totale di features (questo perché, se è uguale, 
     # vuol dir non è stata trovata corrispondenza. Es: num_features_database_image = 568; se indices[i] = 568 vuol dire che per il descrittore in quella
     # posizione delle query locations, non è stato trovato alcuni match all'interno della "descriptor_matching_threshold")
     
-    database_image_locations_to_use = np.array([database_image_locations[indices[i],] for i in range(num_features_query) 
-                                if indices[i] != num_features_database_image])
+    matching_image_locations = np.array([image_locations[indices[i],] for i in range(num_features_query) 
+                                if indices[i] != num_features_image])
     # qua fa la stessa cosa, prendendo in considerazione i descrittori corretti in funzione dell'indice rilasciato restituito dal cKDTree
     # (non considera quelli in cui indices[i] != num_features_database_image perché significa che non c'è stato un matching)
 
-    print(f"query_locations_to_use.shape {query_locations_to_use.shape}")
-    print(f"database_image_locations_to_use.shape {database_image_locations_to_use.shape}")
+
+    print(f"KDTree matching: {matching_query_locations.shape[0]}")
 
 
     # If there are not enough putative matches, early return 0.
-    if query_locations_to_use.shape[0] <= _MIN_RANSAC_SAMPLES:
+    if matching_query_locations.shape[0] <= _MIN_RANSAC_SAMPLES:
         return 0
 
     # Perform geometric verification using RANSAC.
     _, inliers = measure.ransac(
-        (database_image_locations_to_use, query_locations_to_use),
-        transform.AffineTransform,
+        (matching_image_locations, matching_query_locations),
+        transform.PolynomialTransform,
         min_samples=_MIN_RANSAC_SAMPLES,
         residual_threshold=ransac_residual_threshold,
         max_trials=_NUM_RANSAC_TRIALS,
-        random_state=ransac_seed)
+        random_state=0)
 
     if inliers is None:
         inliers = []
-    # se gli passiamo anche le immagini, ce le plotta
     elif query_im_array is not None and index_im_array is not None:
-        if query_im_scale_factors is None:
-            query_im_scale_factors = [1.0, 1.0]
-        if index_im_scale_factors is None:
-            index_im_scale_factors = [1.0, 1.0]
-            inlier_idxs = np.nonzero(inliers)[0]
-        _, ax = plt.subplots()
-        ax.axis('off')
-        ax.xaxis.set_major_locator(plt.NullLocator())
-        ax.yaxis.set_major_locator(plt.NullLocator())
-        plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-        plt.margins(0, 0)
-        feature.plot_matches(
-            ax,
-            query_im_array,
-            index_im_array,
-            query_locations_to_use * query_im_scale_factors,
-            database_image_locations_to_use * index_im_scale_factors,
-            np.column_stack((inlier_idxs, inlier_idxs)),
-            only_matches=True)
-
-    # match_viz_io = io.BytesIO()
-    # plt.savefig(match_viz_io, format='jpeg', bbox_inches='tight', pad_inches=0)
-    # match_viz_bytes = match_viz_io.getvalue()
-
-    # return sum(inliers), match_viz_bytes
+      plot_inlier_lines(query_im_array, index_im_array, matching_query_locations, matching_image_locations, inliers)
+        
     return sum(inliers)
 
 
