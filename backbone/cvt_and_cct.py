8330fb46aa95c9d9235ed54a5207e59e48cebe8e
GabriG23 <greco.gabriele.1996@gmail.com>
2023-06-19 15:21:13 +0200

    Update cvt_and_cct.py

--- backbone/cvt_and_cct.py
+++ backbone/cvt_and_cct.py
@@ -9,7 +9,7 @@ def cvt_initialization(fc_output_dim):
     return CompactTransformer(224, 16, fc_output_dim, depth=12, heads=12, dim_head=64)           # fc_output_dim era il numero delle classi
  
 def cct_initialization(fc_output_dim):
-    return CompactTransformer(224, 16, fc_output_dim, conv_embed=True, depth=6, heads=6, dim_head=32) # dim = 768
+    return CompactTransformer(224, 16, fc_output_dim, conv_embed=True, depth=6, heads=6, dim_head=64) # dim = 768
 
 class CompactTransformer(nn.Module):
     def __init__(self, image_size, patch_size, dim, depth, heads, dim_head, pool='cls', in_channels=3, scale_dim = 0.4, dropout=0.1, emb_dropout=0.1, conv_embed=False):
