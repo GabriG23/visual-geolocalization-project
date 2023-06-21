17658335866eb26bc45cfbf03081078041ea599c
Davide Aiello <dvdaiello98@gmail.com>
2023-06-21 15:56:44 +0200

    preparing for delivery

--- eval.py
+++ eval.py
@@ -5,14 +5,13 @@ import logging
 import multiprocessing
 from datetime import datetime
 
-import test, test_reranked
+import test_reranked
 import parser
 import commons
 from model import network
 from datasets.test_dataset import TestDataset
 
-torch.backends.cudnn.benchmark = True  # Provides a speedup
-
+torch.backends.cudnn.benchmark = True 
 args = parser.parse_arguments(is_training=False)
 start_time = datetime.now()
 output_folder = f"logs/{args.save_dir}/{start_time.strftime('%Y-%m-%d_%H-%M-%S')}"
@@ -23,13 +22,13 @@ logging.info(f"Arguments: {args}")
 logging.info(f"The outputs are being saved in {output_folder}")
 
 #### Model
-model = network.GeoLocalizationNet(args.backbone, args.fc_output_dim, args.fm_reduction_dim, args.reduction)               # istanzia il modello
+model = network.GeoLocalizationNet(args.backbone, args.fc_output_dim, args.fm_reduction_dim, args.reduction)           
 
 logging.info(f"There are {torch.cuda.device_count()} GPUs and {multiprocessing.cpu_count()} CPUs.")
 
 if args.resume_model is not None:
     logging.info(f"Loading model from {args.resume_model}")
-    model_state_dict = torch.load(args.resume_model)                                  # carica il modello
+    model_state_dict = torch.load(args.resume_model)                       
     model.load_state_dict(model_state_dict)
 else:
     logging.info("WARNING: You didn't provide a path to resume the model (--resume_model parameter). " +
@@ -40,6 +39,6 @@ model = model.to(args.device)
 test_ds = TestDataset(args.test_set_folder, queries_folder="queries",
                       positive_dist_threshold=args.positive_dist_threshold)
 
-recalls, recalls_str = test_reranked.test(args, test_ds, model)
-
-# logging.info(f"{test_ds}: {recalls_str}")
+recalls, recalls_str, reranked_recalls, reranked_recalls_str = test_reranked.test(args, test_ds, model)  
+logging.info(f"{test_ds}: Recalls /t: {recalls_str}")
+logging.info(f"{test_ds}: Reranked Recalls /t: {reranked_recalls_str}")
--- test_reranked.py
+++ test_reranked.py
@@ -14,49 +14,49 @@ from local_features_utils import retrieve_locations_and_descriptors, match_featu
 RECALL_VALUES = [1, 5, 10, 20]
 
 
-def test(args: Namespace, eval_ds: Dataset, model: torch.nn.Module) -> Tuple[np.ndarray, str]:      # restituisce l'array con le recall e un stringa che riporta i valori
+def test(args: Namespace, eval_ds: Dataset, model: torch.nn.Module) -> Tuple[np.ndarray, str]:      
     """Compute descriptors of the given dataset and compute the recalls."""
     
-    model = model.eval()                                        # si mette il modello in evaluation mode
-    with torch.no_grad():                                       # all'interno del ciclo, il gradient è disabilitato (requires_grad=False)
+    model = model.eval()                                        
+    with torch.no_grad():                                      
         logging.debug("Extracting database descriptors for evaluation/testing")
-        database_subset_ds = Subset(eval_ds, list(range(eval_ds.database_num)))             # subset del dataset da valutare non considerando le immagini di query
+        database_subset_ds = Subset(eval_ds, list(range(eval_ds.database_num)))        
         database_dataloader = DataLoader(dataset=database_subset_ds, num_workers=args.num_workers,
-                                         batch_size=args.infer_batch_size, pin_memory=(args.device == "cuda"))  # creazione del dataloader in grado di iterare sul dataset
-        all_descriptors = np.empty((len(eval_ds), args.fc_output_dim), dtype="float32")     # ritorna un vettore non inizializzato con una riga per ogni sample da valutare
+                                         batch_size=args.infer_batch_size, pin_memory=(args.device == "cuda"))  
+        all_descriptors = np.empty((len(eval_ds), args.fc_output_dim), dtype="float32")     
         all_local_descriptors = np.empty((len(eval_ds), args.fm_reduction_dim, 14, 14), dtype="float32")
         all_att_prob = np.empty((len(eval_ds), 1, 14, 14), dtype="float32")
         
         
-        for images, indices in tqdm(database_dataloader, ncols=100):                        # e un numero di colonne pari alla dimensione di descrittori
-            global_descriptors, _, _, _, local_descriptors, attn_scores = model(images.to(args.device))                                     # mette le immagini su device e ne calcola il risultato del MODELLO -> i descrittori
-            global_descriptors = global_descriptors.cpu().numpy()                                         # porta i descrittori su cpu e li traforma da tensori ad array
+        for images, indices in tqdm(database_dataloader, ncols=100):                       
+            global_descriptors, _, _, _, local_descriptors, attn_scores = model(images.to(args.device))                                     
+            global_descriptors = global_descriptors.cpu().numpy()                                       
             local_descriptors = local_descriptors.cpu().numpy() 
             attn_scores = attn_scores.cpu().numpy() 
-            all_descriptors[indices.numpy(), :] = global_descriptors                               # riempie l'array mettendo ad ogni indice il descrittore calcolato
+            all_descriptors[indices.numpy(), :] = global_descriptors                               
             all_local_descriptors[indices.numpy(), :] = local_descriptors
             all_att_prob[indices.numpy(), :] = attn_scores
 
 
         logging.debug("Extracting queries descriptors for evaluation/testing using batch size 1")
-        queries_infer_batch_size = 1                                                        # sembra che venga valutata un'immagine per volta
-        queries_subset_ds = Subset(eval_ds, list(range(eval_ds.database_num, eval_ds.database_num+eval_ds.queries_num)))        # in questo caso, crea un subset con sole query
+        queries_infer_batch_size = 1                                                        
+        queries_subset_ds = Subset(eval_ds, list(range(eval_ds.database_num, eval_ds.database_num+eval_ds.queries_num)))      
         queries_dataloader = DataLoader(dataset=queries_subset_ds, num_workers=args.num_workers,
-                                        batch_size=queries_infer_batch_size, pin_memory=(args.device == "cuda"))        # crea il dataloader associato a questo secondo subset
+                                        batch_size=queries_infer_batch_size, pin_memory=(args.device == "cuda"))        
         for images, indices in tqdm(queries_dataloader, ncols=100):
-            global_descriptors, _, _, _, local_descriptors, attn_scores = model(images.to(args.device))             # fa lo stesso lavoro precedente, calcolando per ogni immagine di query il descrittore
+            global_descriptors, _, _, _, local_descriptors, attn_scores = model(images.to(args.device))           
             global_descriptors = global_descriptors.cpu().numpy()
             local_descriptors = local_descriptors.cpu().numpy() 
             attn_scores = attn_scores.cpu().numpy() 
-            all_descriptors[indices.numpy(), :] = global_descriptors                               # riempie l'array mettendo ad ogni indice il descrittore calcolato
+            all_descriptors[indices.numpy(), :] = global_descriptors                             
             all_local_descriptors[indices.numpy(), :] = local_descriptors
-            all_att_prob[indices.numpy(), :] = attn_scores      # rimepiendo il vettore all_descriptors 
-    
-    queries_global_descriptors = all_descriptors[eval_ds.database_num:]    # divide i descrittori delle queries
+            all_att_prob[indices.numpy(), :] = attn_scores      
+
+    queries_global_descriptors = all_descriptors[eval_ds.database_num:]   
     queries_local_descriptors = all_local_descriptors[eval_ds.database_num:]
     queries_att_prob = all_att_prob[eval_ds.database_num:]
 
-    database_global_descriptors = all_descriptors[:eval_ds.database_num]   # dai descrittori del database di immagini da classificare
+    database_global_descriptors = all_descriptors[:eval_ds.database_num]   
     database_local_descriptors = all_local_descriptors[:eval_ds.database_num]
     database_att_prob = all_att_prob[:eval_ds.database_num]
                                                             
@@ -64,61 +64,47 @@ def test(args: Namespace, eval_ds: Dataset, model: torch.nn.Module) -> Tuple[np.
     faiss_index.add(database_global_descriptors)                 
     del all_local_descriptors, all_descriptors, all_att_prob          
     
-    RANSAC = False
+   
     logging.debug("Calculating recalls")
     distances, predictions = faiss_index.search(queries_global_descriptors, max(RECALL_VALUES))    
-    positives_per_query = eval_ds.get_positives() 
-    for k in [80, 85, 90, 95]:
-        for kd_th in [0.55, 0.6, 0.65]:
-            for ransac_th in [10.0, 15.0, 16.0]:
-                recalls = np.zeros(len(RECALL_VALUES))    
-                reranked_recalls = np.zeros(len(RECALL_VALUES))                
-                for query_index, preds in enumerate(tqdm(predictions, ncols=100)):  
-                    reranked_preds = RerankByGeometricVerification(preds, distances[query_index], queries_local_descriptors[query_index], 
-                                                queries_att_prob[query_index], database_local_descriptors[preds], database_att_prob[preds],
-                                                k=k, descriptor_matching_threshold=kd_th, ransac_residual_threshold=ransac_th)
-                    for i, n in enumerate(RECALL_VALUES):           
-                        if np.any(np.in1d(reranked_preds[:n], positives_per_query[query_index])):                                                                                                                         
-                            reranked_recalls[i:] += 1                                               
-                            break     
-                    for i, n in enumerate(RECALL_VALUES):            
-                        if np.any(np.in1d(preds[:n], positives_per_query[query_index])):                                                                                                                                          
-                            recalls[i:] += 1                                             
-                            break                                                           
+    positives_per_query = eval_ds.get_positives()
+    recalls = np.zeros(len(RECALL_VALUES))    
+    reranked_recalls = np.zeros(len(RECALL_VALUES))                
+    for query_index, preds in enumerate(tqdm(predictions, ncols=100)):  
+        reranked_preds = RerankByGeometricVerification(preds, distances[query_index], queries_local_descriptors[query_index], 
+                                    queries_att_prob[query_index], database_local_descriptors[preds], database_att_prob[preds])
+        for i, n in enumerate(RECALL_VALUES):           
+            if np.any(np.in1d(reranked_preds[:n], positives_per_query[query_index])):                                                                                                                         
+                reranked_recalls[i:] += 1                                               
+                break     
+        for i, n in enumerate(RECALL_VALUES):            
+            if np.any(np.in1d(preds[:n], positives_per_query[query_index])):                                                                                                                                          
+                recalls[i:] += 1                                             
+                break                                                           
 
     recalls = recalls / eval_ds.queries_num * 100   
     reranked_recalls = reranked_recalls / eval_ds.queries_num * 100                            
 
     recalls_str = ", ".join([f"R@{val}: {rec:.1f}" for val, rec in zip(RECALL_VALUES, recalls)])  
     reranked_recalls_str = ", ".join([f"R@{val}: {rec:.1f}" for val, rec in zip(RECALL_VALUES, reranked_recalls)])    
-
-    print(f"\n{k = } - {kd_th = } - {ransac_th = } ")
-    print('Recalls \t', recalls_str)
-    print('Reranked Recalls', reranked_recalls_str)
-    print("------------------")
-    return recalls, recalls_str
+    return recalls, recalls_str, reranked_recalls, reranked_recalls_str
 
 
 def RerankByGeometricVerification(query_predictions, distances, query_descriptors, query_attention_prob, 
-                    images_local_descriptors, images_attention_prob, k, descriptor_matching_threshold, ransac_residual_threshold):
+                    images_local_descriptors, images_attention_prob):
    
-    # k = 85
-    # descriptor_matching_threshold=0.6
-    # ransac_residual_threshold=9.0
-    query_locations, query_descriptors = retrieve_locations_and_descriptors(query_attention_prob, query_descriptors, k=k)
+    query_locations, query_descriptors = retrieve_locations_and_descriptors(query_attention_prob, query_descriptors)
 
     inliers_and_initial_scores = []                   
     for i, preds in enumerate(query_predictions):
 
-        image_locations, image_descriptors = retrieve_locations_and_descriptors(images_attention_prob[i], images_local_descriptors[i], k=k)
+        image_locations, image_descriptors = retrieve_locations_and_descriptors(images_attention_prob[i], images_local_descriptors[i])
 
         match_result = match_features(
             query_locations,  
             query_descriptors,
             image_locations,
             image_descriptors,
-            descriptor_matching_threshold,
-            ransac_residual_threshold,
             use_ratio_test=False,
             RANSAC=True)
 
