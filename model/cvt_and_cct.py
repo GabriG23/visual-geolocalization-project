59013d0393dc5c62132ae4f89740b7cd1e6a77c7
GabriG23 <greco.gabriele.1996@gmail.com>
2023-06-25 14:01:38 +0200

    added some comments

--- model/cvt_and_cct.py
+++ model/cvt_and_cct.py
@@ -5,29 +5,29 @@ from einops.layers.torch import Rearrange
 from einops import rearrange
 
 
-def cvt_initialization(fc_output_dim):
-    return CompactTransformer(224, 16, dim=fc_output_dim, depth=12, heads=12)           # fc_output_dim era il numero delle classi
+def cvt_initialization(fc_output_dim): # CVT
+    return CompactTransformer(224, 16, dim=fc_output_dim, depth=12, heads=12) 
  
-def cct_initialization(fc_output_dim):
-    return CompactTransformer(224, 16, dim=fc_output_dim, conv_embed=True, depth=6, heads=4) # dim = 768
+def cct_initialization(fc_output_dim): # CCT
+    return CompactTransformer(224, 16, dim=fc_output_dim, conv_embed=True, depth=6, heads=4)
 
 class CompactTransformer(nn.Module):
     def __init__(self, image_size, patch_size, dim, depth, heads, pool='cls', dim_head=64, scale_dim=4, in_channels=3, dropout=0.1, emb_dropout=0.1, conv_embed=False):
         super().__init__()
 
-        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'  # qui usiamo solo cls, non c'è GeM anche se si potrebbe mettere
 
         assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
         num_patches = (image_size // patch_size) ** 2                       # number of patches 224 // 16 (floor division) = 14
-        patch_dim = in_channels * patch_size ** 2                           # dimesnion of patch -> 3 * 16 ^ 2 = 2304
+        patch_dim = in_channels * patch_size ** 2                           # dimension of patch -> 3 * 16 ^ 2 = 768
 
         if conv_embed:      # convolutional embedding, only for cct
             self.to_patch_embedding = ConvEmbed(in_channels, dim)
             num_patches = self.to_patch_embedding.sequence_length()
-        else:               # sequential embedding, only for cvt
+        else:               # sequential embedding, only for cvt. b batch, c input channels, h height, w weight, p1 p2 patch size
             self.to_patch_embedding = nn.Sequential(
-                Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
-                nn.Linear(patch_dim, dim),
+                Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),  # convert the image in a sequence of non-overlapping patches
+                nn.Linear(patch_dim, dim),  # LINEAR PROJECTION - helps to transform the patch embeddings into a desidred dimension
             )
 
         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, dim))
@@ -35,23 +35,24 @@ class CompactTransformer(nn.Module):
 
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+        self.transformer = Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout) # transformer
 
-        self.pool = nn.Linear(dim, 1)
+        self.pool = nn.Linear(dim, 1)       # seq pool
         self.mlp_head = nn.Sequential(
-            nn.LayerNorm(dim),
-            #nn.Linear(dim, num_classes)
+            nn.LayerNorm(dim),              # normalization
+            #nn.Linear(dim, num_classes)    # took out linear layer, cosface does this
         )
         self.apply(self.init_weight)
 
     def forward(self, img):
-        x = self.to_patch_embedding(img)        # apply convolutional or sequential embedding
-        b, n, _ = x.shape
+        x = self.to_patch_embedding(img)        # apply convolutional or sequential embedding, 1st step Embed to patches
+        b, n, _ = x.shape # n is the number of patches x = [batch, patch, embedding]
 
-        x += self.pos_embedding[:, :(n + 1)]    # nn.Parameter(torch.randn(1, num_patches, dim))
-        x = self.dropout(x)                     # nn.Dropout(emb_dropout)
+        x += self.pos_embedding[:, :(n + 1)]    # learnable parameter of shape [1, num_patches, dim]. each patch embedding in x is addes to its corresponding position embedding
+                                                # aiuta ad incorporare le informazioni spaziali nel patch embeddings, permettendo al modello di catturare relazioni posizionali tra patches 
+        x = self.dropout(x)                     # aiuta a regolarizzare le patch embeddings prima di mandarle al transformers (previene overfitting)
 
-        x = self.transformer(x)                 # Transformer(dim, depth, heads, dim_head, dim*scale_dim, dropout)
+        x = self.transformer(x)                 # 
 
         g = self.pool(x)                        # Sequence Pooling nn.Linear(dim, 1)
         xl = F.softmax(g, dim=1)
@@ -73,34 +74,34 @@ class CompactTransformer(nn.Module):
 
 ##### TRANSFORMER  #####
 class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.1):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.1): # mlp_dim è la dimensione della rete feed-forward
         super().__init__()
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)), # MLHA
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))                           # MLP
             ]))
 
     def forward(self, x):
-        for attn, ff in self.layers:
+        for attn, ff in self.layers: # applica un layer ad x, ogni trasformazione consiste nell'attention (MHSA) e un feedforward (MLP)
             x = attn(x) + x
             x = ff(x) + x
         return x
 
 ##### Feed Forward #####
-class FeedForward(nn.Module):                           # This class defines a feed-forward neural network module with GELU activation and dropout.
+class FeedForward(nn.Module):                    
     def __init__(self, dim, hidden_dim, dropout = 0.):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
+            nn.Linear(dim, hidden_dim),     # linear
+            nn.GELU(),                      # applica GeLU 
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),                 # another linear layer that transforn
-            nn.Dropout(dropout)                         # randomly sets elements of the tensor to zero, helping to prevent overfitting
+            nn.Linear(hidden_dim, dim),     # another linear layer that transforn
+            nn.Dropout(dropout)             # dropout
         )
 
-    def forward(self, x):
+    def forward(self, x): # introduce non-linearity and increse the model capacity to capture complex patterns
         return self.net(x)
 
 ##### Attention Module #####
@@ -108,37 +109,37 @@ class Attention(nn.Module):                             # implements the self-at
     def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
         super().__init__()
         inner_dim = dim_head *  heads                                # 64 * 8 = 512
-        project_out = not (heads == 1 and dim_head == dim)           # 
+        project_out = not (heads == 1 and dim_head == dim)           # ci dice se bisogna fare la proiezione o no
 
-        self.heads = heads                                           # 8
-        self.scale = dim_head ** -0.5                                # 64^(-0.5) = 0.125
+        self.heads = heads                                           
+        self.scale = dim_head ** -0.5     # scaling factor per gli attention scores                            
 
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)    # apply linear module
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)    # mappa x alle queries, keys e valori necessari per il self-attention
 
-        self.to_out = nn.Sequential(
+        self.to_out = nn.Sequential( # riporta l'output alla sua dimensione originale se proj_out è true
             nn.Linear(inner_dim, dim),
             nn.Dropout(dropout)
         ) if project_out else nn.Identity()
 
     def forward(self, x):
-        b, n, _, h = *x.shape, self.heads
-        qkv = self.to_qkv(x)
-        q, k, v = qkv.chunk(3, dim=-1)
-        q = rearrange(q, 'b n (h d) -> b h n d', h=h)
+        b, n, _, h = *x.shape, self.heads       # prende le dimensioni
+        qkv = self.to_qkv(x) # reshape di x con [batch_size, heads, sequence_length, dim_head]
+        q, k, v = qkv.chunk(3, dim=-1)  # il tensore è diviso in queries, keys e values usando chunk sull'ultima dimensione
+        q = rearrange(q, 'b n (h d) -> b h n d', h=h)   #q, k e v vengono rearranged per avere dimensione [batch_size, heads, sequence_length, dim_head]
         k = rearrange(k, 'b n (h d) -> b h n d', h=h)
         v = rearrange(v, 'b n (h d) -> b h n d', h=h)
         
-        dots = torch.einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
+        dots = torch.einsum('b h i d, b h j d -> b h i j', q, k) * self.scale   # applica il dot product tra queries e keys
 
-        attn = torch.softmax(dots, dim=-1)
+        attn = torch.softmax(dots, dim=-1)  # applica softmax sull'ultima dimensione
 
-        out = torch.einsum('b h i j, b h j d -> b h i d', attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        out = self.to_out(out)
+        out = torch.einsum('b h i j, b h j d -> b h i d', attn, v)  # pesa i valori dall'attention score
+        out = rearrange(out, 'b h n d -> b n (h d)') # rearreange per avere dimensione batch_size, sequence_length, inner_dim
+        out = self.to_out(out)  # applica proiezione se proj_out = true
         return out
 
 ##### Residual Block #####
-class Residual(nn.Module):                             # adds the output of a given function to its input
+class Residual(nn.Module):  # enables residuala connection in NN, allowing information from the input to flow through the block unchanged while still incorporating the transformations
     def __init__(self, fn):
         super().__init__()
         self.fn = fn
@@ -147,18 +148,16 @@ class Residual(nn.Module):                             # adds the output of a gi
         return self.fn(x, **kwargs) + x
     
 ##### Pre Normalization #####
-class PreNorm(nn.Module):                              # applies layer normalization before passing the input to a given function
+class PreNorm(nn.Module):       
     def __init__(self, dim, fn):
         super().__init__()
         self.norm = nn.LayerNorm(dim)
         self.fn = fn
 
-    def forward(self, x, **kwargs):
+    def forward(self, x, **kwargs):             # normalizza il tensore di input prima di applicare una funzione, aiuta a stabilizzare il training
         return self.fn(self.norm(x), **kwargs)
     
-
-
-##### CONVOLUTIONAL EMBEDDING #####
+##### CONVOLUTIONAL EMBEDDING MODULE #####
 class ConvEmbed(nn.Module):
     def __init__(self, in_channel, out_channel, kernel_size=7, stride=2, padding=3, pool_kernel_size=3, pool_stride=2, pool_padding=1):
         super(ConvEmbed, self).__init__()
